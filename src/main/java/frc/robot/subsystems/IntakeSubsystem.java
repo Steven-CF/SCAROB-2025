07cffe429b4e4c6dfc7550faae65de493a594f99
Ben Williams <ben@williamshollow.com>
2025-02-08 18:34:40 -0500

    Added some intake code (not finished yet)

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.reduxrobotics.sensors.canandmag.Canandmag;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
@@ -10,7 +11,8 @@ import frc.robot.Constants;
 public class IntakeSubsystem extends SubsystemBase {
     
     private SparkMax intakeMotor = new SparkMax(Constants.Intake.INTAKE_ID, MotorType.kBrushless);
-    private TalonFX pivotMoter = new TalonFX(31);
+    private TalonFX pivotMotor = new TalonFX(31);
+    private Canandmag intakeencoder = new Canandmag(15);
 
     public IntakeSubsystem() {
 
@@ -20,14 +22,24 @@ public class IntakeSubsystem extends SubsystemBase {
         intakeMotor.set(1);
     }
 
-    public void intakePivot() {
-        boolean X;
-        boolean A;
-        if (X) {
-            pivotMoter.set(1);
-        }else if(A) {
-            pivotMoter.set(1);
-        }
+    public void intakePivot(double targetPosition) {
+        // Get the current position of the pivot motor
+        double position = intakeencoder.getAbsPosition();
+
+        // Calculate the error between the target position and the current position
+        double error = targetPosition - position; 
+
+        // Simple proportional control to adjust the intake motor based on the error
+        double kP = 0.1; // Proportional gain, you may need to tune this value
+        double output = kP * error;
+
+        // Set the intake motor speed based on the control output
+        ___.set(output);
+
+        double velocity = intakeencoder.getVelocity(); // Get the velocity of the pivot motor
+
+        intakeencoder.setAbsPosition(0); // Reset the encoder position to 0
+        intakeencoder.setAbsPosition(90); // Set the encoder position to 90
     }
     
 }
