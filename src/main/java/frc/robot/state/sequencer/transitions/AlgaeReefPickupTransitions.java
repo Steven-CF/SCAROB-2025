21a0435e1b9cb591445ff3bf17498928306c7bca
Yoshi <Jesuslv2060@gmail.com>
2025-02-13 17:58:42 -0500

    Stated to work on state A lot of red code need to fix it

---
+++ src/main/java/frc/robot/state/sequencer/transitions/AlgaeReefPickupTransitions.java
@@ -0,0 +1,25 @@
+package frc.robot.state.sequencer.transitions;
+
+import frc.robot.state.sequencer.SequenceInput;
+import frc.robot.state.sequencer.SequenceState;
+
+public class AlgaeReefPickupTransitions {
+    private static final Object transitionTable[][] = {
+        // CURRENT                              INPUT                                     OPERATION                    NEXT
+        {SequenceState.HOME,                    SequenceInput.BEGIN,                      "raiseElevatorNoThreshold",  SequenceState.RAISING_ELEVATOR},
+        {SequenceState.RAISING_ELEVATOR,        SequenceInput.ELEVATOR_DONE,              "prepareToIntake",           SequenceState.INTAKING},
+        {SequenceState.INTAKING,                SequenceInput.BUTTON_RELEASED,            "elevatorSecondStage",       SequenceState.UPDATING_ELEVATOR},
+        {SequenceState.UPDATING_ELEVATOR,       SequenceInput.ELEVATOR_DONE,              "holdAndLower",              SequenceState.LOWERING},
+        {SequenceState.LOWERING,                SequenceInput.ELEVATOR_DONE,              "resetState",                SequenceState.HOME},
+
+        // Level change sequences
+        // TODO implement
+
+        // Abort sequences
+        {SequenceState.RAISING_ELEVATOR,        SequenceInput.BUTTON_RELEASED,            "startReset",               SequenceState.LOWERING}
+    };
+
+    public static Object [][] getTransitionTable() {
+        return transitionTable;
+    }
+}
