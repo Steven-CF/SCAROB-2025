aaebce55d4bd80384ba6772fddda9d9760d83f7c
Yoshi <Jesuslv2060@gmail.com>
2025-03-01 19:07:28 -0500

    added base climb code. Need to fix pid and add follower

---
+++ src/main/java/frc/robot/subsystems/climb/ClimbConstants.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems.climb;
+
+import com.ctre.phoenix6.signals.InvertedValue;
+
+public class ClimbConstants {
+    
+    // climb can ID
+    public final static int climbCanId = 31;
+
+    // motor direction
+    public final static InvertedValue climbMotorDirection = InvertedValue.CounterClockwise_Positive;
+
+    // output
+    //public final static double idleOutput = 0;
+
+    // positions
+    public final static double minClimbPosition = 0;
+    public final static double maxClimbPosition = 0.7;
+    public final static double climbHomePosition = 0;
+    public final static double climbReadyPosition = 0.365;
+    public final static double climbResetThreshold = .1; // indicates when climb is being reset and isClimbing should be set to false
+    public final static double climbArmStowThreshold = .5; // indicates when arm should move out of the way
+    
+}
